d804c1cb1783c51c8328328f99f6043099215a5d
Kim Maida <git@kim-maida.com>
2016-05-24 22:12:39 -0400

    * Clean up unused tutorial code. * Return static message from query string - all is working. * Insert static message string from params into MongoDB.

--- webtask.js
+++ webtask.js
@@ -1,15 +1,24 @@
-var parallel = require('async').parallel;
 var mc = require('mongodb').MongoClient;
+var assert = require('assert');
 
 module.exports = function(context, done) {
 	var query = context.data;
 	var staticMsg = query.message;
 
-	mc.connect(query.mongo, function(error, db) {
-		if (error) {
-			return done(error);
+	mc.connect(query.mongo, function(err, db) {
+		if (err) {
+			return done(err);
 		}
 
-		done(null, 'Hello, ' + query.name + db);
+		assert.equal(null, err);
+
+		db.collection('messages').insertOne({message: staticMsg}, function(err, r) {
+			assert.equal(null, err);
+			assert.equal(1, r.insertedCount);
+
+			db.close();
+		});
+
+		return done(null, staticMsg);
 	});
 };
