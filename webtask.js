fe8ac16269eb7385667a5c135d8151cbb269adf3
Kim Maida <git@kim-maida.com>
2016-05-25 06:59:46 -0400

    * Testing to access GitHub payload.

--- webtask.js
+++ webtask.js
@@ -1,35 +1,34 @@
 var mc = require('mongodb').MongoClient;
 var assert = require('assert');
 
-return function(context, cb) {
+return function(context, callback) {
 	var ctxData = context.data;
 	var payload = context.webhook;
+	var commitsArr = payload.commits;
 
 	if (ctxData.mongo) {
 		mc.connect(ctxData.mongo, function (err, db) {
 			if (err) {
-				cb(err);
+				callback(err);
 			}
 
-			var collection = db.collection('messages');
+			var collection = db.collection('commits');
 
 			assert.equal(null, err);
 
-			// collection.insertOne({todo: todoItem}, function(err, result) {
-			// 	assert.equal(null, err);
-			// 	assert.equal(1, result.insertedCount);
-			//
-			// 	db.close();
-			// });
+			for (var i = 0; i < commitsArr.length; i++) {
+				var commitMsg = commitsArr[i].message;
 
-			console.log(payload);
+				collection.insertOne({commit: commitMsg}, function (err, result) {
+					assert.equal(null, err);
+					assert.equal(1, result.insertedCount);
+				});
+			}
 
 			db.close();
-
-			cb(null, payload);
 		});
 	} else {
 		console.log('MongoDB not provided; could not save');
-		return cb();
+		return callback();
 	}
 };
