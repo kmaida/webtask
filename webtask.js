16dd04ddc3734cdc0d12d605533893724f910b81
Kim Maida <git@kim-maida.com>
2016-05-25 09:42:11 -0400

    * Cleanup and consolidate code. * Change variable name syntax to better reflect their purpose. TODO: complete README.

--- webtask.js
+++ webtask.js
@@ -2,38 +2,35 @@ var mc = require('mongodb').MongoClient;
 var assert = require('assert');
 
 return function(context, callback) {
-	var mdbUri = context.data.mongo;
+	var _MDBURI = context.data.mongo;
 	var ghPayload = context.webhook;
 	var commitsArr = ghPayload.commits;
 
-	if (mdbUri) {
-		mc.connect(mdbUri, function (err, db) {
+	if (_MDBURI) {
+		mc.connect(_MDBURI, function(err, db) {
 			if (err) {
 				callback(err);
 			}
-
 			assert.equal(null, err);
 
-			for (var i = 0; i < commitsArr.length; i++) {
-				var thisCommit = commitsArr[i];
-				var commitMsg = thisCommit.message;
-				var lowerCommit = commitMsg.toLowerCase();
-				var iTodo = lowerCommit.indexOf('todo');
+			for (var _i = 0; _i < commitsArr.length; _i++) {
+				var _thisCommit = commitsArr[_i];
+				var _commitMsg = _thisCommit.message;
+				var _lowerCommit = _commitMsg.toLowerCase();
+				var _iTodo = _lowerCommit.indexOf('todo');
 				var thisTodo = {};
 
-				if (iTodo > -1) {
-					thisTodo.todo = commitMsg.substr(iTodo, commitMsg.length) + ' - ' + thisCommit.author.name;
-				}
+				if (_iTodo > -1) {
+					thisTodo.todo = _commitMsg.substr(_iTodo, _commitMsg.length) + ' - ' + _thisCommit.author.name;
 
-				if (thisTodo.todo) {
-					db.collection('todos').insertOne(thisTodo, function (err, result) {
+					db.collection('todos').insertOne(thisTodo, function(err, result) {
 						assert.equal(null, err);
 						assert.equal(1, result.insertedCount);
-						db.close();
 					});
 				}
 			}
 
+			db.close();
 			return callback(null, { commits: commitsArr });
 		});
 	} else {
