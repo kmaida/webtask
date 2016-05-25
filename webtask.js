f005eddb8aac316084ad5eec7c05a57ba1a8d7aa
Kim Maida <git@kim-maida.com>
2016-05-24 23:36:54 -0400

    * Minor iterations to improve variable names. * Remove callbacks and use HTTP instead.

--- webtask.js
+++ webtask.js
@@ -1,24 +1,28 @@
 var mc = require('mongodb').MongoClient;
 var assert = require('assert');
 
-module.exports = function(context, done) {
-	var query = context.data;
-	var staticMsg = query.message;
+module.exports = function(context, req, res) {
+	var ctxData = context.data;
+	var staticMsg = ctxData.message;
 
-	mc.connect(query.mongo, function(err, db) {
+	mc.connect(ctxData.mongo, function(err, db) {
 		if (err) {
-			return done(err);
+			res.writeHead(500, { 'Content-Type': 'text/html'});
+			res.end('<p>An error occurred.</p>');
 		}
+
+		var collection = db.collection('messages');
 
 		assert.equal(null, err);
 
-		db.collection('messages').insertOne({message: staticMsg}, function(err, r) {
+		collection.insertOne({message: staticMsg}, function(err, result) {
 			assert.equal(null, err);
-			assert.equal(1, r.insertedCount);
+			assert.equal(1, result.insertedCount);
 
 			db.close();
 		});
 
-		return done(null, staticMsg);
+		res.writeHead(200, { 'Content-Type': 'text/html'});
+		res.end('<h1>' + staticMsg + '</h1>');
 	});
 };
